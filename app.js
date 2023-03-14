1e5570768c5d29b7ac2c3ac3a5487661524f490c
HaydenWashburn <hayden.washburn1@gmail.com>
2023-03-14 16:36:02 -0500

    completed exercise 1-2

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
-
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 1) {
+    console.log(`${i} odd`);
+  }
+}
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+for (let i = 1; i < 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+    if (i % 3 === 0) {
+      console.log("Fizz");
+      if (i % 5 === 0) {
+        console.log("Buzz");
+      }
+    }
+  }
+}
